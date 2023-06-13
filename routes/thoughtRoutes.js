7246ac4ff593fc64fca54e0462f11a0d1328b2bd
dExIT <dexit@dyc.lv>
2023-06-13 19:03:08 +0100

    Setting up base structure and models

---
+++ routes/thoughtRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const { Thought, User } = require('../../models');
+
+
+//All thoguhts
+
+router.get('/', async (req, res) => {
+    try {
+        const thoughtData = await Thought.find()
+        res.status(200).json(thoughtData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+//get single thought 
+ router.get('/:id', async (req, res) => {
+    try {
+        const thoughtData = await Thought.findOne({ _id: req.params.id })
+        res.status(200).json(thoughtData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+module.exports = router;
---
+++ routes/userRoutes.js
@@ -0,0 +1,33 @@
+// User Routes
+const router = require('express').Router();
+const { User } = require('../../models');
+
+//All users
+
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.find()
+        res.status(200).json(userData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+//get single user 
+ router.get('/:id', async (req, res) => {
+    try {
+        const userData = await User.findOne({ _id: req.params.id })
+        res.status(200).json(userData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+  
+//post
+
+//put
+
+//delete
+
+//update
+
+module.exports = router;
